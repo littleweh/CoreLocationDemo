91ad5fc2fe625b12f7bc185b9993f086507ef736
littleweh <adakao@gmail.com>
2018-02-24 11:02:50 +0800

    add mapView, tableView

--- LocationDemo/LocationViewController.swift
+++ LocationDemo/LocationViewController.swift
@@ -8,21 +8,68 @@
 
 import UIKit
 import CoreLocation
+import MapKit
 
 class LocationViewController: UIViewController {
     let locationManager = CLLocationManager()
 
+    let mapView = MKMapView()
+    let tableView = UITableView()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.addSubview(mapView)
+        self.view.addSubview(tableView)
+        self.mapView.delegate = self
+        self.tableView.delegate = self
+
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setupMapView()
+        setupTableView()
+
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         locationManager.requestAlwaysAuthorization()
         if CLLocationManager.locationServicesEnabled() {
             locationManager.delegate = self
+
             locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
 //            locationManager.requestLocation()
-            locationManager.startUpdatingLocation()
 //            locationManager.startUpdatingHeading()
-
+            locationManager.startUpdatingLocation()
         }
+
+    }
+
+    func setupMapView() {
+        mapView.mapType = .standard
+        mapView.isZoomEnabled = true
+        mapView.isScrollEnabled = true
+        mapView.showsUserLocation = true
+
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            mapView.leftAnchor.constraint(equalTo: self.view.leftAnchor),
+            mapView.rightAnchor.constraint(equalTo: self.view.rightAnchor),
+            mapView.topAnchor.constraint(equalTo: self.view.layoutMarginsGuide.topAnchor),
+            mapView.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.5)
+        ])
+    }
+
+    func setupTableView() {
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            tableView.leftAnchor.constraint(equalTo: self.view.leftAnchor),
+            tableView.rightAnchor.constraint(equalTo: self.view.rightAnchor),
+            tableView.topAnchor.constraint(equalTo: mapView.bottomAnchor),
+            tableView.bottomAnchor.constraint(equalTo: self.view.layoutMarginsGuide.bottomAnchor)
+        ])
 
     }
 
@@ -35,12 +82,30 @@ extension LocationViewController: CLLocationManagerDelegate {
     }
 
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        let location = locations.first
-        print("coordinate: \(location?.coordinate)")
-        print("altitude: \(location?.altitude) meters")
-        print("timestamp: \(location?.timestamp)")
-        print("speed: \(location?.speed)")
-        print("course: \(location?.course)")
+        guard
+            let location = locations.first as? CLLocation
+            else { return }
+
+        let center = CLLocationCoordinate2D(
+            latitude: location.coordinate.latitude,
+            longitude: location.coordinate.longitude
+        )
+
+        let region = MKCoordinateRegion(
+            center: center,
+            span: MKCoordinateSpan(
+                latitudeDelta: 0.01,
+                longitudeDelta: 0.01
+            )
+        )
+
+        self.mapView.setRegion(region, animated: true)
+
+        print("coordinate: \(location.coordinate)")
+        print("altitude: \(location.altitude) meters")
+        print("timestamp: \(location.timestamp)")
+        print("speed: \(location.speed)")
+        print("course: \(location.course)")
     }
 
     func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
@@ -83,6 +148,14 @@ extension LocationViewController: CLLocationManagerDelegate {
             completion: nil
         )
     }
+
+}
+
+extension LocationViewController: MKMapViewDelegate {
+
+}
+
+extension LocationViewController: UITableViewDelegate {
 
 }
 
